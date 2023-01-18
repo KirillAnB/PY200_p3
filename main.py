10245bf1ad0ab9ecc7efd89a20f733cf33197f07
KirillAnB <it-reso@mail.ru>
2023-01-18 21:15:48 +0300

    Сделано наследование и переопределение

---
+++ main.py
@@ -0,0 +1,42 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.set_pages(pages)
+
+    def set_pages(self, pages):
+        if not isinstance(pages, int):
+            raise TypeError("Check pages type")
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.set_duration(duration)
+
+    def set_duration(self, duration):
+        if not isinstance(duration, float):
+            raise TypeError("Check duration type")
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+if __name__ == "__main__":
+    audio1 = AudioBook('My audio book','Ivan Ivanov',60)
