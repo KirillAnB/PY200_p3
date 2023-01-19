9e9773d3072b1f490872bbced0c5863dd67b9f94
KirillAnB <it-reso@mail.ru>
2023-01-19 15:41:04 +0300

    Атрибуты name и author сделаны приватными,реализованы getter и setter

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self._name = name
-        self._author = author
+        self.__name = name
+        self.__author = author
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def author(self):
+        return self.__author
+
+    @author.setter
+    def author(self, author):
+        self.__author = author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -12,6 +28,7 @@ class Book:
 
 
 class PaperBook(Book):
+    """Класс-конструктор бумажная книга, потомок класса Book"""
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
         self.set_pages(pages)
@@ -26,6 +43,7 @@ class PaperBook(Book):
 
 
 class AudioBook(Book):
+    """Класс-конструктор аудио-книги, потомок класса Book"""
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
         self.set_duration(duration)
@@ -39,4 +57,5 @@ class AudioBook(Book):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
 
 if __name__ == "__main__":
-    audio1 = AudioBook('My audio book','Ivan Ivanov',60)
+    audio1 = AudioBook('My audio book','Ivan Ivanov',60.0)
+    paper1 = PaperBook('My paper book','Petr Petrov', 1000)
